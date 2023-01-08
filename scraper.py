5e93e786e7abd72e2f8590b06aaaa8f3434ff909
Igor Trevelin <igor.trevelin.xavier@gmail.com>
2023-01-08 20:38:42 -0300

    adicionado scraping de vagas de vagas.com.br

--- scraper.py
+++ scraper.py
@@ -1,13 +1,22 @@
 import os
+import re
 import time
 import threading
+import requests
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from chromedriver_py import binary_path
+from joblib import Parallel, delayed
+from bs4 import BeautifulSoup
 
 dumpfolder = os.path.join(os.path.abspath(os.path.dirname(__file__)), "dados")
+writer = pd.ExcelWriter("dados/vagas.xlsx", engine="auto")
+
+"""
+    Web Scraping de vagas publicadas no LinkedIn
+"""
 
 
 def get_linkedin_jobs(n_jobs=1000):
@@ -163,13 +172,101 @@ def get_linkedin_jobs(n_jobs=1000):
         )
 
         job_data["Descrição"] = job_data["Descrição"].str.replace("\n", " ")
-        job_data.to_excel(os.path.join(dumpfolder, "linkedin.xlsx"), index=False)
+        job_data.to_excel(writer, "LinkedIn", index=False)
 
     driver.quit()
 
 
+"""
+    Web Scraping de vagas publicadas no site vagas.com.br
+"""
+
+
+def get_vagascombr_jobs(n_jobs=1000):
+    url = "https://www.vagas.com.br/vagas-de-dados"
+    service_object = Service(binary_path)
+    driver = webdriver.Chrome(service=service_object)
+
+    driver.get(url)
+
+    no_of_jobs = driver.find_element(By.TAG_NAME, "h1").get_attribute("innerText")
+    s = re.search("^(\d+) .*", no_of_jobs)
+    no_of_jobs = int(s.group(1))
+
+    n_jobs = min(n_jobs, no_of_jobs)
+
+    while True:
+        jobs = driver.find_elements(By.CSS_SELECTOR, "li.vaga")
+        print(len(jobs))
+        if len(jobs) >= n_jobs:
+            jobs = jobs[:n_jobs]
+            break
+
+        try:
+            load_more = driver.find_element(By.ID, "maisVagas")
+            load_more.click()
+        except:
+            pass
+        finally:
+            time.sleep(5)
+
+    links = [
+        job.find_element(By.CLASS_NAME, "link-detalhes-vaga").get_attribute("href")
+        for job in jobs
+    ]
+
+    driver.quit()
+
+    def get_job_data(url):
+        try:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            date = soup.find("li", {"class": "job-breadcrumb__item--published"}).text
+            date = re.search("Publicada em (.+)", date).group(1)
+
+            title = soup.find(
+                "h1", {"class": "job-shortdescription__title"}
+            ).text.strip()
+            company = soup.find(
+                "h2", {"class": "job-shortdescription__company"}
+            ).text.strip()
+            description = soup.find(
+                "div", {"class": "job-description__text"}
+            ).text.strip()
+            location = soup.find("span", {"class": "info-localizacao"}).text.strip()
+            seniority = soup.find(
+                "span", {"class": "job-hierarchylist__item--level"}
+            ).text.strip()
+
+            return [date, title, company, description, location, seniority, url]
+        except:
+            return None
+
+    jobs_data = Parallel(n_jobs=5)(delayed(get_job_data)(link) for link in links)
+    jobs_data = list(filter(lambda x: x is not None, jobs_data))
+
+    job_data = pd.DataFrame(
+        jobs_data,
+        columns=[
+            "Data",
+            "Título",
+            "Empresa",
+            "Descrição",
+            "Localização",
+            "Nível",
+            "Link",
+        ],
+    )
+
+    job_data["Descrição"] = job_data["Descrição"].str.replace("\n", " ")
+    job_data.to_excel(writer, "vagas.com.br", index=False)
+
+
 def main():
-    jobs = [get_linkedin_jobs]
+    """
+    Executando jobs de web scraping em paralelo
+    """
+    jobs = [get_linkedin_jobs, get_vagascombr_jobs]
 
     threads = list(map(lambda job: threading.Thread(target=job, daemon=True), jobs))
 
@@ -180,11 +277,12 @@ def main():
         alive = len(list(filter(lambda thread: thread.is_alive(), threads)))
 
         if alive == 0:
-            exit(1)
+            return
 
         time.sleep(5)
 
 
 if __name__ == "__main__":
-    # main()
-    get_linkedin_jobs()
+    main()
+    writer.close()
+    exit(1)
